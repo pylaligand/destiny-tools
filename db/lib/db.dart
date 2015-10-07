5ed108ea01cea1aa3d3d3a4891d3e6e0b6738fcf
P.Y. Laligand <py.laligand@gmail.com>
2015-10-06 21:25:48 -0700

    Library for persisting data in a database.
    Eventually this will be backed by Google Drive. For now only a couple
    of model classes were added - with accompanying tests

---
+++ db/lib/db.dart
@@ -0,0 +1,7 @@
+// Copyright (c) 2015 P.Y. Laligand
+
+library db;
+
+import 'package:collection/equality.dart';
+
+part 'model.dart';
---
+++ db/test/model_test.dart
@@ -0,0 +1,49 @@
+// Copyright (c) 2015 P.Y. Laligand
+
+library model_test;
+
+import 'package:destiny-db/db.dart';
+import 'package:test/test.dart';
+
+main() {
+  var user = new User('gid', 'pid', true, 'bid', 'did');
+  var sameUser = new User('gid', 'pid', true, 'bid', 'did');
+  var differentUser = new User('gid', 'pid', true, 'other_bid', 'did');
+
+  var timestamp = new DateTime.now();
+  var users = [user, differentUser];
+  var db = new Database.withContent(timestamp, users);
+  var sameDb = new Database.withContent(timestamp, users);
+  var differentDb = new Database.withContent(timestamp, [user]);
+
+
+  test('user equality', () {
+    expect(sameUser, equals(user));
+    expect(sameUser.hashCode, equals(user.hashCode));
+  });
+
+  test('user inequality', () {
+    expect(differentUser, isNot(equals(user)));
+  });
+
+  test('user serialization', () {
+    var json = user.toJson();
+    var otherUser = new User.fromJson(json);
+    expect(otherUser, equals(user));
+  });
+
+  test('database equality', () {
+    expect(sameDb, equals(db));
+    expect(sameDb.hashCode, equals(db.hashCode));
+  });
+
+  test('database inequality', () {
+    expect(differentDb, isNot(equals(db)));
+  });
+
+  test('database serialization', () {
+    var json = db.toJson();
+    var otherDb = new Database.fromJson(json);
+    expect(otherDb, equals(db));
+  });
+}
